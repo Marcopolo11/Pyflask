3ef5fca7f8be17844d2ae2094f38d68f992858e0
Marcopolo11 <41788866+Marcopolo11@users.noreply.github.com>
2018-09-03 17:49:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from flask import Flask
-import matplotlib.pyplot as plt
-
 
 app = Flask(__name__)
 
@@ -9,15 +7,6 @@ def hello_world():
   print("Testing Print")
   return 'Hello1, World!'
 
-@app.route('/')
-def build_plot():
-
-    img = io.BytesIO()
-
-    y = [1,2,3,4,5]
-    x = [0,2,1,3,4]
-    a=plt.plot(x,y)
-    return a
 @app.route('/')
 def Test():
     return 'test done'
