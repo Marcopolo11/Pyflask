8dc867443f8282f6d8ee532bcbaf49e29d8fa26d
Marcopolo11 <41788866+Marcopolo11@users.noreply.github.com>
2018-09-04 14:54:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,17 +14,12 @@ def hello_world():
   except Exception as e:
     err=("type error: " + str(e) + a)
     return err
-  
-  img = io.BytesIO()
   y = [1,3]
   x = [0,2]
   #a=matplotlib.pyplot.plot(x,y)
   #return a
-  matplotlib.pyplot.plot(x,y)
-  matplotlib.pyplot.savefig(img, format='png')
-  img.seek(0)
-  plot_url = base64.b64encode(img.getvalue()).decode()
-  return '<img src="data:image/png;base64,{}">'.format(plot_url)
+  return matplotlib.pyplot.plot(x,y)
+
 
 if __name__ == '__main__':
   app.debug = True
