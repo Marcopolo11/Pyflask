06f4f8c9d3abed4f1a5ce072e1411a79b3c46d7b
Marcopolo11 <41788866+Marcopolo11@users.noreply.github.com>
2018-09-04 09:03:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,20 @@ import base64
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-  print("Testing Print")
-  return 'Hello, World!'
+def build_plot():
+    img = io.BytesIO()
+
+    y = [1,2,3,4,5]
+    x = [0,2,1,3,4]
+    a=plt.plot(x,y)
+    plt.savefig(img, format='png')
+    img.seek(0)
+
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    return a
+
 
 if __name__ == '__main__':
     app.run()
