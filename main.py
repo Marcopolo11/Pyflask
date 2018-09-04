3ef6c6b4d2296e18995eac1a74c8ae2c72489ccb
Marcopolo11 <41788866+Marcopolo11@users.noreply.github.com>
2018-09-04 14:44:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from flask import Flask
+import io
+import base64
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hey its Python Flask application!'
+  try:
+    import matplotlib
+  except Exception as e:
+    err=("type error: " + str(e) + a)
+    return err
 
 if __name__ == '__main__':
   app.run()
