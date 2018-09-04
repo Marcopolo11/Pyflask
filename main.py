efcbee6c92319a380ed0e78d559af189fdb9f907
Marcopolo11 <41788866+Marcopolo11@users.noreply.github.com>
2018-09-04 14:33:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = flask.Flask(__name__)
 app.debug = True
 
 
-@app.route('/')
+#@app.route('/')
 def hello_world():
   print("Testing Print")
   a='Hello1'
@@ -23,6 +23,23 @@ def hello_world():
     err=("type error: " + str(e))
     return err
   
+@app.route('/')
+def build_plot():
+    try:
+      img = io.BytesIO()
 
+      y = [1,2,3,4,5]
+      x = [0,2,1,3,4]
+      plt.plot(x,y)
+      plt.savefig(img, format='png')
+      img.seek(0)
+
+      plot_url = base64.b64encode(img.getvalue()).decode()
+
+      return '<img src="data:image/png;base64,{}">'.format(plot_url)
+    except Exception as e:
+      err=("type error: " + str(e) + a)
+      return err
+  
 if __name__ == '__main__':
     app.run()
