55e7ca9c32e1d6e662bd19cf471979d2e27c16ca
Marcopolo11 <41788866+Marcopolo11@users.noreply.github.com>
2018-09-04 17:00:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,20 +8,20 @@ app = Flask(__name__)
 @app.route('/')
 def pan():
    try:
-    import pandas as pd
-  except Exception as e:
-    err=("type error: " + str(e) + a)
-    return err
-  df = pd.DataFrame({'lab':['A', 'B', 'C'], 'val':[10, 30, 20]})
-  ax = df.plot.bar(x='lab', y='val', rot=0)
-  return df
+      import pandas as pd
+   except Exception as e:
+      err=("type error: " + str(e) + a)
+      return err
+   df = pd.DataFrame({'lab':['A', 'B', 'C'], 'val':[10, 30, 20]})
+   ax = df.plot.bar(x='lab', y='val', rot=0)
+   return df
 
-  try:
-    return 'b'
-  except Exception as e:
-    err=("type error: " + str(e) + a)
-    return err
-  return str(y)
+   try:
+      return 'b'
+   except Exception as e:
+      err=("type error: " + str(e) + a)
+      return err
+   return str(y)
 if __name__ == '__main__':
   app.debug = True
   app.run()
