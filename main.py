63ed6f5b7b04dd23623e901dadfe5ecfca1d29f9
Marcopolo11 <41788866+Marcopolo11@users.noreply.github.com>
2018-09-04 09:06:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 from flask import Flask
-from matplotlib import pyplot
+
 import io
 import base64
 
 app = Flask(__name__)
-
-
 @app.route('/')
-def build_plot():
-    img = io.BytesIO()
-
-    y = [1,2,3,4,5]
-    x = [0,2,1,3,4]
-    a=plt.plot(x,y)
-    return a
-
-
+def hello_world():
+  print("Testing Print")
+  return 'Hello, World!'
 if __name__ == '__main__':
     app.run()
