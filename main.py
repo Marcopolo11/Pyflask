0096c696c5e9c3effb4f3694bd3204b7f4b53d00
Marcopolo11 <41788866+Marcopolo11@users.noreply.github.com>
2018-09-04 14:35:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,23 +5,6 @@ import base64
 app = flask.Flask(__name__)
 app.debug = True
 
-
-#@app.route('/')
-def hello_world():
-  print("Testing Print")
-  a='Hello1'
-  b='2World!'
-  try:
-    import matplotlib
-  except Exception as e:
-    err=("type error: " + str(e) + a)
-    return err
-  
-  try:
-    return b
-  except Exception as e:
-    err=("type error: " + str(e))
-    return err
   
 @app.route('/')
 def build_plot():
