50240d4623604b035daaf958bf4732438153eb96
Marcopolo11 <41788866+Marcopolo11@users.noreply.github.com>
2018-09-04 16:53:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def hello_world():
   try:
     import matplotlib.pyplot as plt
-    #return 'fine pyplot'
+    return 'fine pyplot'
       
   except Exception as e:
     err=("type error: " + str(e) + a)
@@ -24,7 +24,7 @@ def hello_world():
   plt.plot()
   a=fig.savefig('a.png')
   try:
-    return a
+    return 'b'
   except Exception as e:
     err=("type error: " + str(e) + a)
     return err
