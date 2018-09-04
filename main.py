92e07d4a8f2e23f4cae2b475786c3138b499de0a
Marcopolo11 <41788866+Marcopolo11@users.noreply.github.com>
2018-09-04 14:53:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,13 @@ def hello_world():
   img = io.BytesIO()
   y = [1,3]
   x = [0,2]
-  a=matplotlib.pyplot.plot(x,y)
-  return a
+  #a=matplotlib.pyplot.plot(x,y)
+  #return a
+  matplotlib.pyplot.plot(x,y)
+  matplotlib.pyplot.savefig(img, format='png')
+  img.seek(0)
+  plot_url = base64.b64encode(img.getvalue()).decode()
+  return '<img src="data:image/png;base64,{}">'.format(plot_url)
 
 if __name__ == '__main__':
   app.debug = True
