4efc8c01a90213bc0aa66df53e062cf78c4ea247
Marcopolo11 <41788866+Marcopolo11@users.noreply.github.com>
2018-09-04 09:23:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,10 @@ import base64
 app = Flask(__name__)
 app.debug = True
 
+
+
+
 @app.route('/')
-def hello_world():
-  print("Testing Print")
-  #return 'Hello1, World!'
-  return flask.redirect('/graphcall')
-
-
-@app.route('/graphcall')
 def graphcall():
   return flask.render_template(other='other', red='redirected!')
 if __name__ == '__main__':
