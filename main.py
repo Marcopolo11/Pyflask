5c9f4185da6c6d536fd09436281870af11253343
Marcopolo11 <41788866+Marcopolo11@users.noreply.github.com>
2018-09-04 09:10:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask
-import pyplot
 import io
 import base64
 
@@ -7,6 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
   print("Testing Print")
-  return 'Hello, World!'
+  return 'Hello1, World!'
 if __name__ == '__main__':
     app.run()
