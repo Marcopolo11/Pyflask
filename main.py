391aafd7706ef6eeeae1e75d372ac264485ea8d7
Marcopolo11 <41788866+Marcopolo11@users.noreply.github.com>
2018-09-04 14:41:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,17 +23,7 @@ app.debug = True
 @app.route('/')
 def build_plot():
     try:
-      img = io.BytesIO()
-
-      y = [1,2,3,4,5]
-      x = [0,2,1,3,4]
-      matplotlib.pyplot.plot(x,y)
-      matplotlib.pyplot.savefig(img, format='png')
-      img.seek(0)
-
-      plot_url = base64.b64encode(img.getvalue()).decode()
-
-      return '<img src="data:image/png;base64,{}">'.format(plot_url)
+      return 'its fine'
     except Exception as e:
       err=("type error: " + str(e) + a)
       return err
