cda57fce1bdacbb0751a970aa55048a705fc6b53
Marcopolo11 <41788866+Marcopolo11@users.noreply.github.com>
2018-09-05 10:57:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 from flask import Flask , render_template
 import io
 import base64
-
+import StringIO
 
 app = Flask(__name__)
 
-@app.route('/')
-def pan():
-   try:
-      import pandasas as pd
-   except Exception as e:
-      err=("type error: " + str(e) + a)
-      return err
-   df = pd.DataFrame({'lab':['A', 'B', 'C'], 'val':[10, 30, 20]})
-   ax = df.plot.bar(x='lab', y='val', rot=0)
-   return df
 
+
+@devices_blueprint.route('/devices/test/')
+def test():
    try:
-      return 'b'
+      import matplotlib.pyplot as plt
+      #return 'fine pyplot'
    except Exception as e:
       err=("type error: " + str(e) + a)
       return err
-   return str(y)
+   img = StringIO.StringIO()
+   y = [1,2,3,4,5]
+   x = [0,2,1,3,4]
+   plt.plot(x,y)
+   plt.savefig(img, format='png')
+   img.seek(0)
+   plot_url = base64.b64encode(img.getvalue())
+   return render_template('test.html', plot_url=plot_url)
+
 if __name__ == '__main__':
   app.debug = True
   app.run()
